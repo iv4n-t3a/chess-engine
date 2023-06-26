65287642eb862026da319f33f541b4cf1f228a69
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2023-06-26 16:15:43 +0300

    Generate new constants for pseudorandom numbers generation

--- src/zobrist.cpp
+++ src/zobrist.cpp
@@ -1,9 +1,9 @@
 #include "zobrist.h"
 
 
-constexpr Hash seed = 43214213132;
-constexpr Hash multiplier = 890523987724;
-constexpr Hash summand = 435235234524352;
+constexpr Hash seed = 6952444504781965567;
+constexpr Hash multiplier = 6716316357005353353;
+constexpr Hash summand = 3581212759251489814;
 
 constexpr Hash next_random(Hash previous) {
 	return previous*multiplier + summand;
@@ -20,7 +20,7 @@ constexpr std::array< std::array<Hash, 64>, 12 > init_piece_constants() {
 	return r;
 }
 
-constexpr Hash player_const = seed;
+constexpr Hash player_const = 8172504009242867825;
 constexpr std::array< std::array<Hash, 64>, 12 > piece_constants = init_piece_constants();
 
 void Hasher::invert_piece(Square sq, Piece p, Side s) {
